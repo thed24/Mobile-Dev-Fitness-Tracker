f28bb0695959a66a78fcb032f08ab469d5c5171c
Dominic Codespoti <dominic.codespoti@myob.com>
2020-11-01 19:45:34 +1100

    Fix location tracking

--- app/src/main/java/com/mobiledev/fitnesstracker/controllers/LocationManager.kt
+++ app/src/main/java/com/mobiledev/fitnesstracker/controllers/LocationManager.kt
@@ -3,34 +3,54 @@ package com.mobiledev.fitnesstracker.controllers
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
+import android.os.Looper
 import android.util.Log
 import android.view.View
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
 import com.mobiledev.fitnesstracker.R
 
+
 class LocationManager {
     private val debugTag = "MAIN ACTIVITY"
     private val requestPermissionRequestCode = 34
+    private lateinit var mLocationRequest: LocationRequest
+    private lateinit var mLocationCallback: LocationCallback
 
     @SuppressLint("MissingPermission")
-    fun getLastLocation(fusedLocationClient: FusedLocationProviderClient, locationFunction: (m: Location) -> Unit) {
+    fun getLastLocation(
+        fusedLocationClient: FusedLocationProviderClient,
+        locationFunction: (m: Location) -> Unit
+    ) {
+        mLocationCallback = object : LocationCallback(){}
+        createLocationRequest()
+        fusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper())
         fusedLocationClient.lastLocation.addOnCompleteListener { taskLocation ->
             if (taskLocation.isSuccessful && taskLocation.result != null) {
                 val location = taskLocation.result
-                if (location != null) {
+                if (location != null)
                     locationFunction(location)
-                    fusedLocationClient.flushLocations()
-                }
             } else {
                 Log.d(debugTag, taskLocation.exception.toString())
             }
         }
         Log.d(debugTag, "Listener added")
+    }
+
+    private fun createLocationRequest() {
+        mLocationRequest = LocationRequest()
+        mLocationRequest.interval = 10000
+        mLocationRequest.fastestInterval = 5000
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
     }
 
     fun checkPermissions(activity: Activity) =
@@ -40,7 +60,10 @@ class LocationManager {
         ) == PackageManager.PERMISSION_GRANTED
 
     fun requestPermissions(activity: Activity) {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.ACCESS_FINE_LOCATION)) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                activity,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )) {
             callSnackBar(
                 R.string.permission_rationale,
                 android.R.string.ok,
